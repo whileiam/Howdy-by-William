7a35db8d13db17501ccaabf05f5e01b9f88c4c1f
Exodus182 <81522633+Exodus182@users.noreply.github.com>
2022-05-29 20:50:22 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class extends Migration 
 {
     /**
      * Run the migrations.
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('password');
             $table->longText('address')->nullable();
             $table->string('phone_number')->nullable();
+            $table->string('role')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
